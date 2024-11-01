1f25384d851553cfc4aa3a67f1a679915cdf8cad
Monee Machado <monee.machado@gmail.com>
2024-11-01 08:54:01 -0600

    ice-cream-shop (ONLY arrays)

---
+++ app.js
@@ -0,0 +1,72 @@
+const toppings = [
+  {
+    name: 'Chocolate Drizzle',
+    price: 0.50,
+    quantity: 0
+  },
+
+  {
+    name: 'Peanut Butter',
+    price: 1.00,
+    quantity: 0
+  },
+
+  {
+    name: 'Sprinkles',
+    price: 0.75,
+    quantity: 0
+  }
+
+]
+//# REGION (explain the hotkey for easier implementation)
+// NOTE single ticks between strings for this one, but do ASK what the difference is between "", '', and ``!
+
+const flavors = [
+  {
+    name: 'Vanilla Swirl',
+    price: 2.00,
+    quantity: 0
+  },
+
+  {
+    name: 'Mint and Chip',
+    price: 3.75,
+    quantity: 0
+  },
+
+  {
+    name: 'Neapolitan',
+    price: 2.50,
+    quantity: 0
+  }
+
+]
+//# REGION                 
+
+const cones = [
+  {
+    name: 'Dipped Chocolate Cone',
+    price: 2.80,
+    quantity: 0
+  },
+
+  {
+    name: 'Bubble Waffle Cone',
+    price: 4.40,
+    quantity: 0
+  },
+
+  {
+    name: 'Hand-Rolled Sugar Cone',
+    price: 1.50,
+    quantity: 0
+  }
+
+]
+//# REGION           
+
+function addToppings() {
+  toppings[0]
+}
+
+// NOTE does the WHITE txt above signify that no action will be taking place in the console log? (Taking reference from the fruit stand tutorial and am not sure if the difference is in the color layout of VS)
